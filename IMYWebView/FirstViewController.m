0939aa9e47823c991559b976fe3ef36e12830dc5
yanshiyu <superjobman@163.com>
2015-10-27 16:27:50 +0800

    webView

---
+++ IMYWebView/FirstViewController.m
@@ -0,0 +1,50 @@
+//
+//  FirstViewController.m
+//  IMYWebView
+//
+//  Created by ljh on 15/7/3.
+//  Copyright (c) 2015年 IMY. All rights reserved.
+//
+
+#import "FirstViewController.h"
+#import "IMYWebView.h"
+
+@interface FirstViewController ()
+@property(strong,nonatomic)IMYWebView* webView;
+@end
+
+@implementation FirstViewController
+-(instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if(self)
+    {
+        self.title = @"IMYWebView";
+    }
+    return self;
+}
+- (void)viewDidLoad
+{
+    self.view.backgroundColor = [UIColor whiteColor];
+    [super viewDidLoad];
+    self.webView = [[IMYWebView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:_webView];
+    
+    if(_webView.usingUIWebView)
+    {
+        self.title = @"ClickRefresh-UIWebView";
+    }
+    else
+    {
+        self.title = @"ClickRefresh-WKWebView";
+    }
+    
+    [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.tmall.com"]]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ IMYWebView/SecondViewController.m
@@ -0,0 +1,49 @@
+//
+//  SecondViewController.m
+//  IMYWebView
+//
+//  Created by ljh on 15/7/3.
+//  Copyright (c) 2015年 IMY. All rights reserved.
+//
+
+#import "SecondViewController.h"
+#import "IMYWebView.h"
+@interface SecondViewController ()
+@property(strong,nonatomic)IMYWebView* webView;
+@end
+
+@implementation SecondViewController
+-(instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if(self)
+    {
+        self.title = @"UIWebView";
+    }
+    return self;
+}
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.webView = [[IMYWebView alloc] initWithFrame:self.view.bounds usingUIWebView:YES];
+    [self.view addSubview:_webView];
+    
+    if(_webView.usingUIWebView)
+    {
+        self.title = @"ClickRefresh-UIWebView";
+    }
+    else
+    {
+        self.title = @"ClickRefresh-WKWebView";
+    }
+    
+    [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.tmall.com"]]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
